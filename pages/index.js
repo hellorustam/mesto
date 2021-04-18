01a85574750bf9df06d525282d6115c8e54bb814
Rustam Samiev <blakfoh@gmail.com>
2021-04-18 11:25:39 +0300

    создал апи-конфиг и изменяю класс апи

--- pages/index.js
+++ pages/index.js
@@ -33,11 +33,29 @@ const formMesto = new FormValidator(validationConfig, formAdd);
 
 const popupWithImage = new PopupWithImage(popupImg);
 
-const api = new Api({
-  address: "https://mesto.nomoreparties.co/v1",
-  token: "1f3f6d46-ee23-42d9-b041-2bb6b8e9765e",
-  groupID: "cohort-22",
-});
+const api = new Api({});
+
+// const api = new Api({
+//   address: "https://mesto.nomoreparties.co/v1",
+//   token: "1f3f6d46-ee23-42d9-b041-2bb6b8e9765e",
+//   groupID: "cohort-22",
+//   // name: userData.name,
+//   // about: userData.about,
+// });
+
+// api.getUserData().then((data) => {
+//   // renderUserDataInContent(data);
+//   profileNameNode.textContent = data?.name;
+//   profileAboutNode.textContent = data?.about;
+//   return data;
+// });
+
+function renderUserDataInContent(data) {
+  profileNameNode.textContent = data.name;
+  profileAboutNode.textContent = data.about;
+  profileAvatar.src = "";
+  profileAvatar.style.backgroundImage = `url('${data.avatar}')`;
+}
 
 const userData = {};
 
@@ -46,33 +64,58 @@ api
   .then((data) => {
     userData.name = data.name;
     userData.about = data.about;
-    // userData.avatar = data.avatar;
-    const { name, about, avatar } = data;
-    profileNameNode.textContent = name;
-    profileAboutNode.textContent = about;
-    profileAvatar.src = "";
-    profileAvatar.style.backgroundImage = `url('${avatar}')`;
+    userData.avatar = data.avatar;
+    // const { name, about, avatar } = data;
+    renderUserDataInContent(data);
   })
   .catch((err) =>
     console.log("Ошибка при получении данных о пользователе: " + err)
   );
 
-// profileNameNode.textContent = userData.name;
-// profileAboutNode.textContent = about;
-
-// console.log(profileNameNode.textContent);
-
 console.log(userData);
 
-// profileNameNode.textContent = userData.then((data) => {
-//   const { name, about, avatar } = data;
-//   profileNameNode.textContent = name;
-//   profileAboutNode.textContent = about;
-// });
+// const newInitialCards = [];
 
-api
-  .getCards()
-  .catch((err) => console.log("Ошибка при получении карточек: " + err));
+// api
+//   .getCards()
+//   .then((data) => {
+//     data.forEach((element) => {
+//       newInitialCards.push(element);
+//     });
+
+//     console.log(newInitialCards);
+//     console.log(newInitialCards[2]);
+//   })
+//   .catch((err) => console.log("Ошибка при получении карточек: " + err));
+
+// const newInitialCards = api
+//   .getCards()
+//   .then((data) => {
+//     // return Promise.resolve(data);
+//     return data;
+//   })
+//   .catch((err) => console.log("Ошибка при получении карточек: " + err));
+
+// console.log(newInitialCards);
+
+// async function newInitialCards() {
+//   return await api
+//     .getCards()
+//     .catch((err) => console.log("Ошибка при получении карточек: " + err));
+// }
+
+// newInitialCards().then(console.log(data));
+
+//----
+
+// const newUserData =
+// api
+//   .changeUserData()
+//   // .then((data) => console.log(data))
+//   .catch((err) => console.log("Ошибка при получении карточек: " + err));
+
+// console.log(newUserData);
+// console.log(newUserData.then((data) => data));
 
 function openPopup(dataCard) {
   popupWithImage.openPopup(dataCard);
@@ -85,6 +128,7 @@ const createCard = (data) => {
 const section = new Section(
   {
     items: initialCards,
+    // items: newInitialCards(),
     renderer: (item) => {
       section.addItem(createCard(item));
     },
@@ -119,7 +163,7 @@ userInfo.setUserInfo({
 
 // Вызов попапа редактирования профиля
 editButtonNode.addEventListener("click", () => {
-  const getUserInfo = userInfo.getUserInfo();
+  // const getUserInfo = userInfo.getUserInfo();
 
   nameInput.value = userData.name;
   aboutInput.value = userData.about;
