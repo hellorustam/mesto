c4e169c3676e88e07ea9b737d25e2ca2eec1acad
Rustam Samiev <blakfoh@gmail.com>
2021-04-25 16:39:20 +0300

    Вывод данных пользователя на верстку

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,11 +1,9 @@
 export class Api {
-  constructor(address, token, groupID) {
+  constructor({ address, token, groupID }) {
     this._address = address;
     this._token = token;
     this._groupID = groupID;
   }
-
-  // ----
 
   getUserData() {
     return fetch(`${this._address}/${this._groupID}/users/me`, {
@@ -20,108 +18,4 @@ export class Api {
       return Promise.reject(`Ошибка: ${response.status}`);
     });
   }
-
-  changeUserData(data) {
-    return fetch(`${this._address}/${this._groupID}/users/me`, {
-      method: "PATCH",
-      headers: {
-        authorization: this._token,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-  }
-
-  changeAvatarData(data) {
-    return fetch(`${this._address}/${this._groupID}/users/me/avatar`, {
-      method: "PATCH",
-      headers: {
-        authorization: this._token,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-  }
-
-  // ----
-
-  getCards() {
-    return fetch(`${this._address}/${this._groupID}/cards`, {
-      headers: {
-        authorization: this._token,
-      },
-    }).then((response) => {
-      if (response.ok) {
-        return response.json();
-      }
-      return Promise.reject(`Ошибка: ${response.status}`);
-    });
-  }
-
-  postCard(data) {
-    return fetch(`${this._address}/${this._groupID}/cards`, {
-      method: "POST",
-      headers: {
-        authorization: this._token,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    }).then((response) => {
-      if (response.ok) {
-        return response.json();
-      }
-      return Promise.reject(`Ошибка: ${response.status}`);
-    });
-  }
-
-  // ----
-
-  addLikeCard(id) {
-    return fetch(`${this._address}/${this._groupID}/cards/likes/${id}`, {
-      method: "PUT",
-      headers: {
-        authorization: this._token,
-        "Content-Type": "application/json",
-      },
-    }).then((response) => {
-      if (response.ok) {
-        return response.json();
-      }
-      return Promise.reject(`Ошибка: ${response.status}`);
-    });
-  }
-
-  removeLikeCard(id) {
-    return fetch(`${this._address}/${this._groupID}/cards/likes/${id}`, {
-      method: "DELETE",
-      headers: {
-        authorization: this._token,
-        "Content-Type": "application/json",
-      },
-    }).then((response) => {
-      if (response.ok) {
-        return response.json();
-      }
-      return Promise.reject(`Ошибка: ${response.status}`);
-    });
-  }
-
-  // ----
-
-  deleteCard(id) {
-    return fetch(`${this._address}/${this._groupID}/cards/${id}`, {
-      method: "DELETE",
-      headers: {
-        authorization: this._token,
-        "Content-Type": "application/json",
-      },
-    }).then((response) => {
-      if (response.ok) {
-        return response.json();
-      }
-      return Promise.reject(`Ошибка: ${response.status}`);
-    });
-  }
-
-  // ----
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import {
   editButtonNode,
   profileNameNode,
   profileAboutNode,
+  profileAvatar,
   addButtonNode,
   popupProfileNode,
   nameInput,
@@ -21,7 +22,8 @@ import { PopupWithForm } from "../components/PopupWithForm.js";
 import { FormValidator } from "../components/FormValidator.js";
 import { UserInfo } from "../components/UserInfo.js";
 import { Section } from "../components/Section.js";
-import { PopupWithImage } from "../components/PopupWithImage";
+import { PopupWithImage } from "../components/PopupWithImage.js";
+import { Api } from "../components/Api.js";
 
 const fromEdit = document.querySelector(validationConfig.popUpProfileForm);
 const formProfile = new FormValidator(validationConfig, fromEdit);
@@ -30,6 +32,39 @@ const formAdd = document.querySelector(validationConfig.popUpMestoForm);
 const formMesto = new FormValidator(validationConfig, formAdd);
 
 const popupWithImage = new PopupWithImage(popupImg);
+
+const api = new Api({
+  address: "https://mesto.nomoreparties.co/v1",
+  token: "1f3f6d46-ee23-42d9-b041-2bb6b8e9765e",
+  groupID: "cohort-22",
+});
+
+// ----
+
+api
+  .getUserData()
+  .then((data) => {
+    profileNameNode.textContent = data?.name;
+    profileAboutNode.textContent = data?.about;
+    profileAvatar.src = data?.avatar;
+
+    editButtonNode.addEventListener("click", () => {
+      nameInput.value = data.name;
+      aboutInput.value = data.about;
+
+      profilePopup.openPopup();
+    });
+
+    // Вызов попапа смены аватара
+    // profileEditAvatar.addEventListener("click", () => {
+    //   avatarPopup.openPopup();
+    // });
+  })
+  .catch((err) =>
+    console.log("Ошибка при получении данных о пользователе: " + err)
+  );
+
+// ----
 
 function openPopup(dataCard) {
   popupWithImage.openPopup(dataCard);
@@ -72,16 +107,6 @@ const userInfo = new UserInfo(profileNameNode, profileAboutNode);
 userInfo.setUserInfo({
   name: profileNameNode.textContent,
   about: profileAboutNode.textContent,
-});
-
-// Вызов попапа редактирования профиля
-editButtonNode.addEventListener("click", () => {
-  const getUserInfo = userInfo.getUserInfo();
-
-  nameInput.value = getUserInfo.name;
-  aboutInput.value = getUserInfo.about;
-
-  profilePopup.openPopup(nameInput, aboutInput);
 });
 
 // ----
