eea021a132249be7d9978484a13de559a3338d0a
Rustam Samiev <blakfoh@gmail.com>
2021-04-26 22:00:12 +0300

    Лайк и не лайк карточки

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -40,6 +40,8 @@ const selectorsObj = {
   mestoTitleInput: ".popup__mesto-title",
   mestoLinkInput: ".popup__mesto-link",
   mestoFormElement: ".popup__mesto-form",
+  elementsLike: ".elements__like",
+  // countLike: ".elements__count_like",
 
   popupImg: ".popup-img",
   popupImgSource: ".popup__img",
@@ -47,6 +49,8 @@ const selectorsObj = {
 
   cardsContainer: ".elements",
   cardTemplate: "#card-template",
+
+  userId: "746c6052f7a7f26f04c96054",
 };
 
 //---
@@ -73,6 +77,10 @@ const closeButtonAddNode = popupAddNode.querySelector(
 const mestoTitleInput = qSel(selectorsObj.mestoTitleInput);
 const mestoLinkInput = qSel(selectorsObj.mestoLinkInput);
 const mestoFormElement = qSel(selectorsObj.mestoFormElement);
+const elementsLike = qSel(selectorsObj.elementsLike);
+// const countLike = selectorsObj.elementsLike.querySelector(
+//   selectorsObj.countLike
+// );
 
 const popupImg = qSel(selectorsObj.popupImg);
 const popupImgSource = qSel(selectorsObj.popupImgSource);
@@ -102,6 +110,8 @@ export {
   mestoTitleInput,
   mestoLinkInput,
   mestoFormElement,
+  elementsLike,
+  // countLike,
   popupImg,
   popupImgSource,
   popupImgCaption,
