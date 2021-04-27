2b9d1700dd175c8d6472aa5e7fd7611fa8a53583
Rustam Samiev <blakfoh@gmail.com>
2021-04-27 22:31:39 +0300

    Отмена перезагрузки страницы при удалении карточки

--- src/components/PopupDelCard.js
+++ src/components/PopupDelCard.js
@@ -9,7 +9,8 @@ export class PopupDelCard extends Popup {
   setEventListeners() {
     this._form = this._popup.querySelector(".popup__form");
 
-    this._form.addEventListener("submit", () => {
+    this._form.addEventListener("submit", (evt) => {
+      evt.preventDefault();
       this._handleSubmit();
     });
     super.setEventListeners();
